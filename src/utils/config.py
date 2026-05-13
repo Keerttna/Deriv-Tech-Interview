547c3ca3acc307fcce754795f1f5d383f55a1c73
Keerttna <127674493+Keerttna@users.noreply.github.com>
2026-05-13 16:48:31 +0530

    Initialize funnel friction analytics pipeline skeleton

---
+++ src/utils/config.py
@@ -0,0 +1,16 @@
+FUNNEL_STAGES = [
+    "landing",
+    "signup_form",
+    "email_verify",
+    "kyc_doc_upload",
+    "kyc_review",
+    "first_deposit",
+    "first_trade"
+]
+
+OUTPUT_DIR = "outputs"
+
+RANDOM_SEED = 42
+
+TARGET_EVENTS = 5000
+TARGET_SESSIONS = 600
